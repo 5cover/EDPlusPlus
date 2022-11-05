546e2eeddaa3097b5a1f842db434e9ce7c30c4dc
Scover <thediscover22450@gmail.com>
2022-11-05 08:54:26 +0100

    Vue et modèle

---
+++ bdd.py
@@ -0,0 +1,107 @@
+import sqlite3
+from datetime import date
+from collections import defaultdict
+
+# Établissement d'une connection avec le fichier de base de donnée.
+conn = sqlite3.connect("dbed.db")
+# Création du curseur, un objet qui permet de parcourir la BDD et d'exécuter des requêtes SQL.
+cur = conn.cursor()
+
+class Matiere:
+    def __init__(self, id_: str, type_: str, nom: str, nomCourt: str):
+        self.id = id_
+        self.type = type_
+        self.nom = nom
+        self.nomCourt = nomCourt
+    def __str__(self):
+        return f'{self.type} / {self.nom}'
+
+class Evaluation:
+    def __init__(self, id_: str, titre: str, dateEval: date, noteMax: int, coef: float, id_matiere: str):
+        self.id = id_
+        self.titre = titre
+        self.dateEval = dateEval
+        self.noteMax = noteMax
+        self.coef = coef
+        self.id_matiere = id_matiere
+        
+class Eleve:
+    def __init__(self, id_: str, nom: str, prenom: str, id_classe: str):
+        self.id = id_
+        self.nom = nom
+        self.prenom = prenom,
+        self.id_classe = id_classe
+        
+    def __str__(self):
+        # Pour une raison iconnue, self.prenom devient un tuple à 1 élément ici?
+        return f'{self.nom.upper()} {self.prenom[0]}'
+
+def MEM(eleve: Eleve):
+    """ Cette fonction permet d'obtenir un graphique retraçant chaque moyenne pour toutes les matière d'un élève choisi
+    Retour : dictionnaire (dict)
+    clés : matières
+    valeurs : moyenne sur 1 """
+
+    # Obtention de la liste des matières auquelles l'élève est inscrit.
+    matieres = [Matiere(*t) for t in cur.execute(f'SELECT DISTINCT MATIERES.* FROM COURS\
+                                                   INNER JOIN MATIERES on MATIERES.id = id_matiere\
+                                                   WHERE id_eleve = {eleve.id}').fetchall()]
+
+    moyennes = defaultdict(float)
+    
+    for matiere in matieres: # SELECTION DE CHAQUE MATIERE
+        evals = getEvaluations(matiere)
+        if not evals: # Si il n'y a aucune évaluation pour cette matière
+            continue
+        for evaluation in evals:
+            cur.execute(f'SELECT valeur FROM NOTES WHERE id_eleve = {eleve.id} AND id_evaluation = {evaluation.id}')
+            moyennes[matiere] += cur.fetchone()[0] / evaluation.noteMax * evaluation.coef
+        moyennes[matiere] /= sum((evaluation.coef for evaluation in evals))
+
+    return dict(moyennes)
+
+def MNM(matiere: Matiere):
+    """ Retour : dictionnaire (dict)
+            clés : élèves
+            valeurs : moyenne sur 1 """
+    
+    eleves = [Eleve(*t) for t in cur.execute(f'SELECT DISTINCT ELEVES.* FROM COURS\
+                                               INNER JOIN ELEVES on ELEVES.id = id_eleve\
+                                               WHERE id_matiere = {matiere.id}').fetchall()]
+    
+    evals = getEvaluations(matiere)
+    
+    # Dictionnaire avec valeur par défaut (0) pour les clés inconnues
+    moyennes = defaultdict(float)
+    
+    # On commence par remplir les moyennes avec le numérateur
+    for evaluation in evals:
+        for eleve in eleves:
+            # Obtient la valeur de la note de l'élève dans l'évaluation
+            cur.execute(f'SELECT valeur FROM NOTES where id_eleve={eleve.id} AND id_evaluation={evaluation.id}')
+            # Il n'y a qu'un seul résultat. On divise par noteMax pour obtenir la note sur 1.
+            moyennes[eleve] += cur.fetchone()[0] / evaluation.noteMax * evaluation.coef
+    
+    # On calcule la moyenne pondérée en divisant par la somme des coefficients.
+    sommeCoefs = sum((evaluation.coef for evaluation in evals))
+    for eleve in eleves:
+        moyennes[eleve] /= sommeCoefs
+    
+    return dict(moyennes)
+
+def getEvaluations(matiere):
+    return [Evaluation(t[0],
+                       t[1],
+                       date(*map(int, t[2].split('-'))),
+                       int(t[3]),
+                       float(t[4]),
+                       t[5])
+            for t in cur.execute(f'SELECT * FROM EVALUATIONS WHERE id_matiere={matiere.id}').fetchall()]
+
+def getEleves():
+    return [Eleve(*t) for t in cur.execute('SELECT * from ELEVES').fetchall()]
+
+def getMatieres():
+    return [Matiere(*t) for t in cur.execute('SELECT * from MATIERES').fetchall()]
+
+    
---
+++ vue.py
@@ -0,0 +1,60 @@
+import bdd
+import tkinter as tk
+import tkinter.ttk as ttk
+import matplotlib.pyplot as plt
+
+def creerFenetre():
+    # La note maximale à utiliser pour les moyennes
+    NOTE_MAX = 20
+    def MEM(eleve: bdd.Eleve):
+        """ Affiche le graphique MEM. """
+        res = bdd.MEM(eleve)
+        plt.bar([key.nomCourt for key in res.keys()], [val * NOTE_MAX for val in res.values()])
+        plt.suptitle(f"Moyennes de {eleve}")
+        plt.xticks(rotation='30')
+        plt.show()
+        
+    def MNM(matiere: bdd.Matiere):
+        """ Affiche le graphique MNM. """
+        res = bdd.MNM(matiere)
+        plt.bar(res.keys(), [val * NOTE_MAX for val in res.values()])
+        plt.suptitle(f"Moyennes en {matiere}")
+        plt.show()
+        
+    def NEM(eleve: bdd.Eleve):
+        """ Affiche le graphique NEM. """
+        pass
+    
+    eleves = bdd.getEleves()
+    matieres = bdd.getMatieres()
+    
+    def creerComboboxEleve():
+        return ttk.Combobox(values = eleves, state = 'normal')
+    def creerComboboxMatiere():
+        return ttk.Combobox(values = matieres, state = 'normal')
+    def grille(widget, r, c):
+        widget.grid(sticky="ew", row=r, column=c, padx=3, pady=6)
+        
+    root = tk.Tk();
+    root.title("ED++")
+    root.geometry('480x640')
+    root.iconbitmap("icon.ico")
+    
+    for i in range(4):
+        root.rowconfigure(i, weight=1)
+    for i in range(2):
+        root.columnconfigure(i, weight=1)
+        
+    nem = creerComboboxEleve()
+    grille(nem, 0, 0)
+    grille(ttk.Button(text="Note d’un élève pour une matière", command=lambda: NEM(eleves[nem.current()])), 0, 1)
+    
+    mem = creerComboboxEleve()
+    grille(mem, 1, 0)
+    grille(ttk.Button(text="Moyenne d’un élève par matière", command=lambda: MEM(eleves[mem.current()])), 1, 1)
+    
+    mnm = creerComboboxMatiere()
+    grille(mnm, 2, 0)
+    grille(ttk.Button(text="Moyenne des notes pour une matière", command=lambda: MNM(matieres[mnm.current()])), 2, 1)
+    
+    root.mainloop()
